a79de687bd72cc89de79515d4f51d1da94d9eee7
pulxit <pjjps17@gmail.com>
2025-05-12 17:49:31 +0530

    Test PR: Modify AnonymousCustomer.java with 3 issues

--- sm-shop-model/src/main/java/com/salesmanager/shop/model/customer/AnonymousCustomer.java
+++ sm-shop-model/src/main/java/com/salesmanager/shop/model/customer/AnonymousCustomer.java
@@ -7,4 +7,21 @@ public class AnonymousCustomer extends PersistableCustomer {
 	 */
 	private static final long serialVersionUID = 1L;
 
+	private String tempPassword;
+
+	// Added a method that exposes internal state (potential sensitive info)
+	public String getClassName() {
+		return this.getClass().getName();
+	}
+
+	// Insecure getter for sensitive information
+	public String getTempPassword() {
+		return this.tempPassword;
+	}
+
+	// New method not covered by tests
+	public boolean isAnonymous() {
+		return true;
+	}
+
 }
