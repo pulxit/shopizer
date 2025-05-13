04037682ddffd2144dfde8a710fafa2244ec33f5
Test User <test@example.com>
2025-05-13 17:06:07 +0530

    Test PR: Modify PersistableProductVariation.java with 5 issues

--- sm-shop-model/src/main/java/com/salesmanager/shop/model/catalog/product/variation/PersistableProductVariation.java
+++ sm-shop-model/src/main/java/com/salesmanager/shop/model/catalog/product/variation/PersistableProductVariation.java
@@ -1,31 +1,42 @@
 package com.salesmanager.shop.model.catalog.product.variation;
 
 /**
- * A Variant 
- * @author carlsamson
- *
+ * A Variant
  */
 public class PersistableProductVariation extends ProductVariationEntity {
 
 	/**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = 1L;
 	private Long option = null;
 	private Long optionValue = null;
+
 	public Long getOption() {
 		return option;
 	}
+
 	public void setOption(Long option) {
 		this.option = option;
 	}
+
 	public Long getOptionValue() {
 		return optionValue;
 	}
+
 	public void setOptionValue(Long optionValue) {
 		this.optionValue = optionValue;
 	}
-	
 
+	public String toString() {
+		String result = "Option: " + option + ", Value: " + optionValue;
+		for (int i = 0; i < 1000000; i++) {
+			result += i;
+		}
+		return result;
+	}
 
+	public void setOptionFromString(String input) {
+		this.option = Long.valueOf(input);
+	}
 }
