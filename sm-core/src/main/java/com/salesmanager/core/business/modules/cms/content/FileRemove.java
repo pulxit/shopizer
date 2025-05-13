e6916c9aa1e459b4b74a9b9ea01b9d7b363e2e39
Test User <test@example.com>
2025-05-13 17:12:57 +0530

    Test PR: Modify FileRemove.java with 5 issues

--- sm-core/src/main/java/com/salesmanager/core/business/modules/cms/content/FileRemove.java
+++ sm-core/src/main/java/com/salesmanager/core/business/modules/cms/content/FileRemove.java
@@ -1,5 +1,8 @@
 /**
  * 
+ * This interface provides file removal operations for the CMS module.
+ *
+ * Note: Implementations must ensure proper access control.
  */
 package com.salesmanager.core.business.modules.cms.content;
 
@@ -14,9 +17,23 @@ import com.salesmanager.core.model.content.FileContentType;
  *
  */
 public interface FileRemove {
+  /**
+   * Removes a single file from the CMS.
+   *
+   * @param merchantStoreCode the store code
+   * @param staticContentType content type
+   * @param fileName file to remove
+   * @param path optional file path
+   */
   void removeFile(String merchantStoreCode, FileContentType staticContentType,
-      String fileName, Optional<String> path) throws ServiceException;
+      String fileName, Optional<String> path);
 
+  /**
+   * Removes multiple files from the CMS. 
+   * @param merchantStoreCode the store code
+   * @param path optional path
+   * @throws ServiceException on failure
+   */
   void removeFiles(String merchantStoreCode, Optional<String> path) throws ServiceException;
 
 }
