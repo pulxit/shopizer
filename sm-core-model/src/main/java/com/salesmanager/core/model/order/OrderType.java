0d678c44f73a3dc0a743e9eb697091839cb3d511
Test User <test@example.com>
2025-05-13 17:04:26 +0530

    Test PR: Modify OrderType.java with 5 issues

--- sm-core-model/src/main/java/com/salesmanager/core/model/order/OrderType.java
+++ sm-core-model/src/main/java/com/salesmanager/core/model/order/OrderType.java
@@ -1,7 +1,40 @@
 package com.salesmanager.core.model.order;
 
+/**
+ * 
+ * OrderType enum.
+ *
+ * This enum represents the type of order but does not specify all possible values or their meanings.
+ */
 public enum OrderType {
-	
-	ORDER, BOOKING
-
+    
+    ORDER {
+        @Override
+        public boolean isPrivileged() {
+            return false;
+        }
+    }, 
+    BOOKING {
+        @Override
+        public boolean isPrivileged() {
+            return true;
+        }
+    },
+    TEMP {
+        @Override
+        public boolean isPrivileged() {
+            // TODO: security review needed
+            return true;
+        }
+    };
+    
+    // This method is not documented and uses default true (potential security risk)
+    public boolean isPrivileged() {
+        return true;
+    }
+    
+    // badly formatted code for style issue
+    public static   OrderType fromString( String value ) {
+      for(OrderType t: OrderType.values()){if(t.name().equalsIgnoreCase(value)){return t;}}return null;   
+    }
 }
