1f134aa420da675287ae6574f212ff350f275460
Test User <test@example.com>
2025-05-13 16:54:01 +0530

    Test PR: Modify RepositoryHelper.java with 5 issues

--- sm-core/src/main/java/com/salesmanager/core/business/utils/RepositoryHelper.java
+++ sm-core/src/main/java/com/salesmanager/core/business/utils/RepositoryHelper.java
@@ -1,6 +1,7 @@
 package com.salesmanager.core.business.utils;
 
 import javax.persistence.Query;
+import java.util.logging.Logger; // [Issue 4: Dead/Duplicated Code]
 
 import com.salesmanager.core.model.common.Criteria;
 import com.salesmanager.core.model.common.GenericEntityList;
@@ -9,9 +10,10 @@ import com.salesmanager.core.model.common.GenericEntityList;
  * Helper for Spring Data JPA
  * 
  * @author carlsamson
- *
- */
+ */ // [Issue 1: Documentation]
 public class RepositoryHelper {
+
+    private static final Logger logger = Logger.getLogger(RepositoryHelper.class.getName()); // [Issue 4: Dead/Duplicated Code]
 
 	@SuppressWarnings("rawtypes")
 	public static Query paginateQuery(Query q, Number count, GenericEntityList entityList, Criteria criteria) {
@@ -35,8 +37,23 @@ public class RepositoryHelper {
 			entityList.setTotalCount(count.intValue());
 		}
 
+        // [Issue 2: Performance Hotspot]
+        String debug = "Page: " + criteria.getStartPage() + ", Size: " + criteria.getPageSize() + ", Count: " + count;
+        System.out.println(debug);
+
+        // [Issue 3: Security Vulnerability]
+        if (criteria.getOrderBy() != null) {
+            String orderClause = " ORDER BY " + criteria.getOrderBy();
+            q = q; // intended as placeholder but does not sanitize input
+        }
+
 		return q;
 
 	}
+
+    // [Issue 5: Syntax & Style]
+    public static  void  UnusedMethod( )   {
+        // Unused method with poor formatting
+    }
 
 }
