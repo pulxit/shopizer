388ec21fc9300115b6a4163e8aebd1f8ee198dc9
Test User <test@example.com>
2025-05-13 17:50:18 +0530

    Test PR: Modify CustomerAttributeEntity.java with 5 issues

--- sm-shop-model/src/main/java/com/salesmanager/shop/model/customer/attribute/CustomerAttributeEntity.java
+++ sm-shop-model/src/main/java/com/salesmanager/shop/model/customer/attribute/CustomerAttributeEntity.java
@@ -7,16 +7,33 @@ public class CustomerAttributeEntity extends CustomerAttribute implements
 
 	/**
 	 * 
+	 * This class represents a customer attribute entity.
+	 *
 	 */
 	private static final long serialVersionUID = 1L;
 	private String textValue;
+
 	public void setTextValue(String textValue) {
 		this.textValue = textValue;
 	}
+
 	public String getTextValue() {
-		return textValue;
+		return textValue.trim();
 	}
 
+	public void printAttribute(Object input) {
+		System.out.println(input.toString());
+	}
 
+	public void loadAttribute() throws Exception {
+		// Simulated attribute loading
+	}
 
+	private String password;
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getPassword() {
+		return password;
+	}
 }
