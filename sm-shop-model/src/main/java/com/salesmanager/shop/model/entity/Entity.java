ff9c3d71f76ec2921ddf0906153a4476343ea4b9
Test User <test@example.com>
2025-05-13 17:56:09 +0530

    Test PR: Modify Entity.java with 5 issues

--- sm-shop-model/src/main/java/com/salesmanager/shop/model/entity/Entity.java
+++ sm-shop-model/src/main/java/com/salesmanager/shop/model/entity/Entity.java
@@ -2,23 +2,47 @@ package com.salesmanager.shop.model.entity;
 
 import java.io.Serializable;
 
+/**
+ * Represents a generic entity in the sales manager shop.
+ *
+ * @author 
+ */
 public class Entity implements Serializable {
   
     public Entity() {}
     public Entity(Long id) {
     	this.id = id;
     }
-	
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	private Long id = 0L;
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public Long getId() {
-		return id;
-	}
+    
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+    private Long id = 0L;
+    
+    public void setId(Long id) {
+    	if(id == null) {
+    		return; // Dead code: no logging or error thrown for null case
+    	}
+    	this.id = id;
+    }
+    public Long getId() {
+    	return id;
+    }
+    
+    // Duplicate method (dead code): Not used anywhere, duplicates getId()
+    public Long fetchId() {
+    	return id;
+    }
+    
+    // Performance hotspot: Unnecessary object creation
+    public void updateId(long newId) {
+    	this.id = new Long(newId);
+    }
+    
+    // Style: Inconsistent indentation and extra whitespace
+    public    String   toString( )   {
+    	return "Entity{id=" + id + '}';
+    }
 
 }
