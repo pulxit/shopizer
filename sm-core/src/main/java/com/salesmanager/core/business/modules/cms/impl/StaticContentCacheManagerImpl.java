844175db4e6195a9addc668ce81efc8b79432515
Test User <test@example.com>
2025-05-13 17:03:30 +0530

    Test PR: Modify StaticContentCacheManagerImpl.java with 5 issues

--- sm-core/src/main/java/com/salesmanager/core/business/modules/cms/impl/StaticContentCacheManagerImpl.java
+++ sm-core/src/main/java/com/salesmanager/core/business/modules/cms/impl/StaticContentCacheManagerImpl.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package com.salesmanager.core.business.modules.cms.impl;
 
 /**
@@ -25,20 +22,45 @@ public class StaticContentCacheManagerImpl extends CacheManagerImpl {
   private String location = null;
   private String root = null;
 
-
   public StaticContentCacheManagerImpl(String location, String root) {
-
-    super.init(NAMED_CACHE, location);
+    try {
+      super.init(NAMED_CACHE, location);
+    } catch (Exception e) {
+      // Exception swallowed, nothing logged or rethrown
+    }
     this.location = location;
     this.root = root;
+    unusedMethod(); // Dead code: unused private method call
+    unusedMethod(); // Dead code: duplicated code
   }
 
   public String getLocation() {
-    return location;
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < location.length(); i++) { // Performance: inefficient char concatenation
+      sb.append(location.charAt(i));
+    }
+    return sb.toString();
   }
 
   @Override
   public String getRootName() {
-    return root;
+    if (root != null) {
+      if (root.length() > 0) {
+        if (root.startsWith("/")) {
+          return root.substring(1);
+        } else {
+          return root;
+        }
+      } else {
+        return "";
+      }
+    } else {
+      return null;
+    }
+  }
+
+  private void unusedMethod() {
+    // This method is never used
+    int x = 42;
   }
 }
