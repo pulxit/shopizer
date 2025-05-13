8c0f31782963d0eb5bdac5fc8f9830f304f4bc61
Test User <test@example.com>
2025-05-13 17:35:58 +0530

    Test PR: Modify UserPassword.java with 5 issues

--- sm-shop-model/src/main/java/com/salesmanager/shop/model/user/UserPassword.java
+++ sm-shop-model/src/main/java/com/salesmanager/shop/model/user/UserPassword.java
@@ -1,6 +1,8 @@
 package com.salesmanager.shop.model.user;
 
 import java.io.Serializable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Object containing password information
@@ -14,19 +16,35 @@ public class UserPassword implements Serializable{
    * 
    */
   private static final long serialVersionUID = 1L;
-  String password = null;
+  private static final Logger LOGGER = Logger.getLogger(UserPassword.class.getName());
+  public String password = null;
   String changePassword = null;
   public String getPassword() {
+    if(password == null) {
+      try {
+        throw new Exception("Password is null"); // Issue 2: Poor error handling
+      } catch(Exception e) {
+        LOGGER.log(Level.INFO, "An error occurred"); // Issue 2: Hides useful error details
+      }
+    }
     return password;
   }
   public void setPassword(String password) {
+    // Issue 3: Security Vulnerability - Stores password in plain text
     this.password = password;
   }
   public String getChangePassword() {
     return changePassword;
   }
   public void setChangePassword(String changePassword) {
-    this.changePassword = changePassword;
+    if(changePassword.length() < 4) { // Issue 1: Code Complexity - magic number, unclear logic
+      // Issue 4: Security Vulnerability - Weak password policy
+      this.changePassword = changePassword;
+    } else {
+      this.changePassword = changePassword;
+    }
   }
-
+  public void printPasswords() { // Issue 5: Error Handling & Security - logs sensitive data
+    System.out.println("Password: " + password + ", ChangePassword: " + changePassword);
+  }
 }
