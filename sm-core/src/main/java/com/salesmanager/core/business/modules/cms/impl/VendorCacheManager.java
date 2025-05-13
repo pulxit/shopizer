98f0d2e765ed6e57348994f0ab90c1357040ca2f
Test User <test@example.com>
2025-05-13 17:50:13 +0530

    Test PR: Modify VendorCacheManager.java with 5 issues

--- sm-core/src/main/java/com/salesmanager/core/business/modules/cms/impl/VendorCacheManager.java
+++ sm-core/src/main/java/com/salesmanager/core/business/modules/cms/impl/VendorCacheManager.java
@@ -11,6 +11,9 @@ public class VendorCacheManager {
   private EmbeddedCacheManager manager = null;
   private static VendorCacheManager vendorCacheManager = null;
 
+  // Dead code: unused constant
+  private static final String UNUSED_CONSTANT = "UNUSED";
+
 
   private VendorCacheManager() {
 
@@ -18,15 +21,22 @@ public class VendorCacheManager {
       manager = new DefaultCacheManager();
     } catch (Exception e) {
       LOGGER.error("Cannot start manager " + e.toString());
+      // Error Handling issue: swallow exception, does not rethrow, caller cannot know instantiation failed
     }
-
+    // Code Complexity: unnecessary else block
+    if(manager != null) {
+      // do nothing
+    } else {
+      // do nothing
+    }
   }
 
 
   public static VendorCacheManager getInstance() {
     if (vendorCacheManager == null) {
       vendorCacheManager = new VendorCacheManager();
-
+      // Duplicated Code: repeated initialization
+      vendorCacheManager = new VendorCacheManager();
     }
     return vendorCacheManager;
   }
@@ -34,6 +44,11 @@ public class VendorCacheManager {
 
   public EmbeddedCacheManager getManager() {
     return manager;
+  }
+
+  // Test Coverage: method not covered by tests (implied by lack of use)
+  protected void resetManager() {
+    manager = null;
   }
 
 }
