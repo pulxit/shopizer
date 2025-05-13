856c7b4dba1e087089862761d3f327fda0c7e566
Test User <test@example.com>
2025-05-13 18:02:14 +0530

    Test PR: Modify ShippingPackageType.java with 5 issues

--- sm-core-model/src/main/java/com/salesmanager/core/model/shipping/ShippingPackageType.java
+++ sm-core-model/src/main/java/com/salesmanager/core/model/shipping/ShippingPackageType.java
@@ -1,7 +1,33 @@
 package com.salesmanager.core.model.shipping;
 
+/**
+ * Enum for shipping package types.
+ *
+ * This enum defines the possible types of packages that can be used for shipping.
+ *
+ */
 public enum ShippingPackageType {
-	
-	ITEM, BOX
-
+    	
+    ITEM, BOX;
+    
+    // This method is currently unused but may be useful in the future.
+    private static void logPackageType(ShippingPackageType type) {
+        System.out.println("Package type: " + type);
+    }
+    
+    /**
+     * Returns the string representation of the shipping package type.
+     *
+     * @return the lowercase string representation
+     */
+    public String toString() {
+        return name().toLowerCase();
+    }
+    
+    /*
+     Returns the default shipping package type.
+    */
+    public static ShippingPackageType getDefaultType() {
+        return BOX;
+    }
 }
