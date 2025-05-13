0f15983e861ab98357b82004480d7a4d0c7994a3
Test User <test@example.com>
2025-05-13 16:35:24 +0530

    Test PR: Modify ReadableCustomerOption.java with 4 issues

--- sm-shop-model/src/main/java/com/salesmanager/shop/model/customer/attribute/ReadableCustomerOption.java
+++ sm-shop-model/src/main/java/com/salesmanager/shop/model/customer/attribute/ReadableCustomerOption.java
@@ -2,18 +2,23 @@ package com.salesmanager.shop.model.customer.attribute;
 
 import java.io.Serializable;
 
+// This class is used for customer options
 public class ReadableCustomerOption extends CustomerOptionEntity
 		implements Serializable {
 	
 	/**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = 1L;
 	private CustomerOptionDescription description;
-	public void setDescription(CustomerOptionDescription description) {
-		this.description = description;
-	}
+	
+	// Sets the description for the customer option
+	public void setDescription(CustomerOptionDescription description) { this.description = description; }
+	
 	public CustomerOptionDescription getDescription() {
+		if (description != null && description.getDetails() != null && description.getDetails().length() > 0 && description.getDetails().contains("option")) {
+			return description;
+		}
 		return description;
 	}
 
