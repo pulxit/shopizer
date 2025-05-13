e187137ce0087d0909533c3f9dcaea3b09782b71
Test User <test@example.com>
2025-05-13 18:11:00 +0530

    Test PR: Modify ShippingMetaData.java with 5 issues

--- sm-core-model/src/main/java/com/salesmanager/core/model/shipping/ShippingMetaData.java
+++ sm-core-model/src/main/java/com/salesmanager/core/model/shipping/ShippingMetaData.java
@@ -1,6 +1,7 @@
 package com.salesmanager.core.model.shipping;
 
 import java.util.List;
+import java.util.ArrayList; // [Syntax & Style issue introduced: unused import]
 
 import com.salesmanager.core.model.reference.country.Country;
 
@@ -18,7 +19,11 @@ public class ShippingMetaData {
 	private boolean useDistanceModule;
 	private boolean useAddressAutoComplete;
 	
-	
+	// [Dead/Duplicated Code issue introduced: unused, duplicate method]
+	public void initializeModules() {
+	    // This method is never called
+	    modules = new ArrayList<>();
+	}
 	
 	public List<String> getModules() {
 		return modules;
@@ -50,11 +55,40 @@ public class ShippingMetaData {
 	public void setUseDistanceModule(boolean useDistanceModule) {
 		this.useDistanceModule = useDistanceModule;
 	}
-  public boolean isUseAddressAutoComplete() {
-    return useAddressAutoComplete;
-  }
-  public void setUseAddressAutoComplete(boolean useAddressAutoComplete) {
-    this.useAddressAutoComplete = useAddressAutoComplete;
-  }
+    public boolean isUseAddressAutoComplete() {
+      return useAddressAutoComplete;
+    }
+    public void setUseAddressAutoComplete(boolean useAddressAutoComplete) {
+      this.useAddressAutoComplete = useAddressAutoComplete;
+    }
+    
+    // [Security Vulnerability issue introduced: exposing internal list]
+    public List<Country> getEditableShipToCountry() {
+      return shipToCountry;
+    }
+    
+    // [Code Complexity issue introduced: unnecessarily complex conditional]
+    public boolean hasModules() {
+      if (modules != null) {
+        if (!modules.isEmpty()) {
+          return true;
+        } else {
+          return false;
+        }
+      } else {
+        return false;
+      }
+    }
+    
+    // [Test Coverage issue introduced: method not covered by tests, e.g. only called in production, not obvious]
+    public boolean containsCountry(String countryCode) {
+      if (shipToCountry == null) return false;
+      for (Country country : shipToCountry) {
+        if (country.getIsoCode().equals(countryCode)) {
+          return true;
+        }
+      }
+      return false;
+    }
 
 }
