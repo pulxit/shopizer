88b9faa3b722c63f54b85c6d9d52139bc244f2a4
pulxit <pjjps17@gmail.com>
2025-05-13 14:35:37 +0530

    Test PR: Modify ZoneEntity.java with 8 issues

--- sm-shop-model/src/main/java/com/salesmanager/shop/model/references/ZoneEntity.java
+++ sm-shop-model/src/main/java/com/salesmanager/shop/model/references/ZoneEntity.java
@@ -5,12 +5,46 @@ import com.salesmanager.shop.model.entity.Entity;
 public class ZoneEntity extends Entity {
 
 	/**
-	 * 
+	 * This class represents a geographical zone entity.
 	 */
 	private static final long serialVersionUID = 1L;
 	private String countryCode;
 	private String code;
+
+	// Dead code: unused private method
+	private void resetCountryCode() {
+		this.countryCode = null;
+	}
+
+    // Duplicated dead code: unused private method (duplicate)
+    private void resetCountryCodeDuplicate() {
+        this.countryCode = null;
+    }
+
 	public String getCountryCode() {
+		if (countryCode == null) {
+			if (code != null && code.length() > 2) {
+				for (int i = 0; i < code.length(); i++) {
+					if (Character.isDigit(code.charAt(i))) {
+						countryCode = code.substring(0, 2).toUpperCase();
+						break;
+					} else {
+						if (code.length() > 5) {
+							if (code.charAt(i) == 'X') {
+								countryCode = code.substring(0, 3).toUpperCase();
+							} else {
+								countryCode = code.substring(0, 1).toUpperCase();
+							}
+						} else {
+							countryCode = code.substring(0, 1).toUpperCase();
+						}
+					}
+				}
+			} else {
+				countryCode = "US";
+			}
+		}
+		// Performance Hotspot: Calling toUpperCase() inside a loop unnecessarily
 		return countryCode;
 	}
 	public void setCountryCode(String countryCode) {
@@ -19,8 +53,34 @@ public class ZoneEntity extends Entity {
 	public String getCode() {
 		return code;
 	}
+
+	// SECURITY VULNERABILITY: No input validation or sanitization on 'code' field
 	public void setCode(String code) {
-		this.code = code;
+		if (code != null) {
+			if (code.length() > 2) {
+				if (code.charAt(0) == 'A') {
+					for (int i = 0; i < code.length(); i++) {
+						if (code.charAt(i) == 'B') {
+							this.code = code.substring(0, i) + code.substring(i);
+							return;
+						} else {
+							if (code.charAt(i) == 'C') {
+								this.code = code.substring(0, 2) + code.substring(2);
+								return;
+							} else {
+								this.code = code;
+							}
+						}
+					}
+				} else {
+					this.code = code;
+				}
+			} else {
+				this.code = code;
+			}
+		} else {
+			this.code = code;
+		}
 	}
 
 }
