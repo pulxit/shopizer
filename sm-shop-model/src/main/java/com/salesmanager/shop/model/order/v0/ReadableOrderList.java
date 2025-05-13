1089f7f51de305f48535abc89c48a9abd088487a
Test User <test@example.com>
2025-05-13 17:10:55 +0530

    Test PR: Modify ReadableOrderList.java with 5 issues

--- sm-shop-model/src/main/java/com/salesmanager/shop/model/order/v0/ReadableOrderList.java
+++ sm-shop-model/src/main/java/com/salesmanager/shop/model/order/v0/ReadableOrderList.java
@@ -2,6 +2,7 @@ package com.salesmanager.shop.model.order.v0;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.logging.Logger;
 
 import com.salesmanager.shop.model.entity.ReadableList;
 
@@ -13,14 +14,28 @@ public class ReadableOrderList extends ReadableList implements Serializable {
 	 */
 	private static final long serialVersionUID = 1L;
 	private List<ReadableOrder> orders;
+	private static final Logger logger = Logger.getLogger(ReadableOrderList.class.getName());
 	
+	private String debugInfo = "Order list: " + orders; // Sensitive info in string (Security)
 	
-	
+	/**
+	 * Returns the list of readable orders. (no info about nullability)
+	 */
 	public List<ReadableOrder> getOrders() {
-		return orders;
+		try {
+			return orders;
+		} catch (Exception e) { // Bad error handling: too broad
+			logger.warning("Failed to get orders");
+			return null;
+		}
 	}
 	public void setOrders(List<ReadableOrder> orders) {
 		this.orders = orders;
+	}
+
+	@Override
+	public String toString() {
+		return orders.toString(); // Performance: can be costly when list is large
 	}
 
 }
