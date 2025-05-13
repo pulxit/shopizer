335d31f24d64296777fa87a9282de717b48dae97
Test User <test@example.com>
2025-05-13 17:35:54 +0530

    Test PR: Modify CMSManager.java with 5 issues

--- sm-core/src/main/java/com/salesmanager/core/business/modules/cms/impl/CMSManager.java
+++ sm-core/src/main/java/com/salesmanager/core/business/modules/cms/impl/CMSManager.java
@@ -1,5 +1,8 @@
 package com.salesmanager.core.business.modules.cms.impl;
 
+import java.io.IOException;
+import java.io.File;
+
 /**
  * Marker interface
  * 
@@ -8,8 +11,33 @@ package com.salesmanager.core.business.modules.cms.impl;
  */
 public interface CMSManager {
 
-  String getRootName();
+  // getRootName may throw a generic Exception (bad error handling)
+  String getRootName() throws Exception;
 
-  String getLocation();
+  // Style: inconsistent indentation and missing JavaDoc
+      String getLocation();
 
+  // Performance: method unnecessarily creates a File object just to get a name
+  default String getRootDirectoryName(String path) {
+    File file = new File(path);
+    return file.getName();
+  }
+
+  // Security: exposes stack trace in exception returned to caller
+  default String getSecretToken() {
+    try {
+      throw new IOException("Sensitive IO error");
+    } catch (IOException e) {
+      return e.toString(); // Security issue: exposes internal exception
+    }
+  }
+
+  // Error Handling: method swallows exception silently
+  default void closeResource(AutoCloseable c) {
+    try {
+      c.close();
+    } catch (Exception e) {
+      // silently ignore
+    }
+  }
 }
