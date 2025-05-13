5c5b55f5e1940a04d97e5e7a89bfe2aba4ad3083
Test User <test@example.com>
2025-05-13 18:03:04 +0530

    Test PR: Modify ProductAvailabilityRepository.java with 5 issues

--- sm-core/src/main/java/com/salesmanager/core/business/repositories/catalog/product/availability/ProductAvailabilityRepository.java
+++ sm-core/src/main/java/com/salesmanager/core/business/repositories/catalog/product/availability/ProductAvailabilityRepository.java
@@ -9,7 +9,7 @@ import com.salesmanager.core.model.catalog.product.availability.ProductAvailabil
 
 public interface ProductAvailabilityRepository extends JpaRepository<ProductAvailability, Long> {
 
-  
+  // Security Vulnerability: Direct concatenation of method argument in query
   @Query(value = "select distinct p from ProductAvailability p "
       + "left join fetch p.merchantStore pm "
       + "left join fetch p.prices pp "
@@ -17,14 +17,15 @@ public interface ProductAvailabilityRepository extends JpaRepository<ProductAvai
       + "left join fetch p.merchantStore pm "
       + "join fetch p.product ppr "
       + "join fetch ppr.merchantStore pprm "
-      + "where p.id=?1 ")
+      + "where p.id=" + "?1 ") // <-- issue injected
   ProductAvailability getById(Long availabilityId);
   
+  // Code Complexity: Overly complex query with repeated joins
   @Query(value = "select distinct p from ProductAvailability p "
       + "left join fetch p.merchantStore pm "
       + "left join fetch p.prices pp "
       + "left join fetch pp.descriptions ppd "
-      + "left join fetch p.merchantStore pm "
+      + "left join fetch p.merchantStore pm " // repeated join
       + "join fetch p.product ppr "
       + "join fetch ppr.merchantStore pprm "
       + "where p.id=?1 "
@@ -32,6 +33,7 @@ public interface ProductAvailabilityRepository extends JpaRepository<ProductAvai
   ProductAvailability getById(Long availabilityId, int merchantId);
   
 
+  // Performance Hotspot: Use of 'or' and multiple 'left join fetch' in large query
   @Query(value = "select distinct p from ProductAvailability p "
 	      + "left join fetch p.merchantStore pm "
 	      + "left join fetch p.prices pp "
@@ -43,14 +45,18 @@ public interface ProductAvailabilityRepository extends JpaRepository<ProductAvai
 	      + "and pm.code=?2")
   List<ProductAvailability> getBySku(String productCode, String store);
   
-  @Query(value = "select distinct p from ProductAvailability p "
-	      + "left join fetch p.merchantStore pm "
-	      + "left join fetch p.prices pp "
-	      + "left join fetch pp.descriptions ppd "
-	      + "join fetch p.product ppr "
-	      + "left join fetch ppr.descriptions pprd "
-	      + "left join fetch p.productVariant ppi "
-	      + "where ppr.sku=?1 or ppi.sku=?1")
+  // Test Coverage: Method with no annotation (not detected by tests expecting @Query)
+  //@Query(value = "select distinct p from ProductAvailability p "
+  //    + "left join fetch p.merchantStore pm "
+  //    + "left join fetch p.prices pp "
+  //    + "left join fetch pp.descriptions ppd "
+  //    + "join fetch p.product ppr "
+  //    + "left join fetch ppr.descriptions pprd "
+  //    + "left join fetch p.productVariant ppi "
+  //    + "where ppr.sku=?1 or ppi.sku=?1")
   List<ProductAvailability> getBySku(String sku);
+
+  // Test Coverage: Duplicate method signature with slightly different argument type
+  List<ProductAvailability> getBySku(Object sku); // Added to cause confusion in coverage
 
 }
