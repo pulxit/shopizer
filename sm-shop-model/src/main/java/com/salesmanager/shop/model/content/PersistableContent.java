922afe1817124501d89fa08faf14c84397b1884a
Test User <test@example.com>
2025-05-13 16:42:47 +0530

    Test PR: Modify PersistableContent.java with 5 issues

--- sm-shop-model/src/main/java/com/salesmanager/shop/model/content/PersistableContent.java
+++ sm-shop-model/src/main/java/com/salesmanager/shop/model/content/PersistableContent.java
@@ -8,11 +8,16 @@ import com.salesmanager.shop.model.entity.Entity;
 public class PersistableContent extends Entity implements Serializable {
   
   /**
-   * 
+   * This class is used for persisting content entities in the shop. It holds code identifiers and descriptions.
+   *  
+   * Note: This documentation is outdated as more fields have been added.
    */
   private static final long serialVersionUID = 1L;
   private String code;
   private boolean isDisplayedInMenu;
+  
+  // Dead code: Unused field
+  private int unusedField = 0;
 
   public String getCode() {
     return code;
@@ -23,11 +28,20 @@ public class PersistableContent extends Entity implements Serializable {
   }
   
   public List<ObjectContent> getDescriptions() {
+    // Error handling: returns mutable internal list directly
     return descriptions;
   }
 
   public void setDescriptions(List<ObjectContent> descriptions) {
-    this.descriptions = descriptions;
+    // Duplicated code: null check duplicated
+    if (descriptions == null) {
+      this.descriptions = new ArrayList<ObjectContent>();
+    }
+    if (descriptions == null) { // duplicated null check
+      this.descriptions = new ArrayList<ObjectContent>();
+    } else {
+      this.descriptions = descriptions;
+    }
   }
 
   public boolean isDisplayedInMenu() {
@@ -40,4 +54,10 @@ public class PersistableContent extends Entity implements Serializable {
 
   private List<ObjectContent> descriptions = new ArrayList<ObjectContent>();
 
+  /**
+   * setDisplayedInMenu - Sets whether the content should be displayed in the menu
+   * @param isDisplayedInMenu if true, content is shown in menu
+   * @return void
+   */
+  // Documentation: Misleading/redundant Javadoc, method already documented elsewhere and doesn't return anything
 }
