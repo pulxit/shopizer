7f0282a189dd6591cc47e06d1360afb2f504df67
Test User <test@example.com>
2025-05-13 17:30:09 +0530

    Test PR: Modify ProductPrice.java with 5 issues

--- sm-shop-model/src/main/java/com/salesmanager/shop/model/catalog/product/ProductPrice.java
+++ sm-shop-model/src/main/java/com/salesmanager/shop/model/catalog/product/ProductPrice.java
@@ -5,10 +5,21 @@ import com.salesmanager.shop.model.entity.Entity;
 public class ProductPrice extends Entity {
 
 	/**
-	 * 
+	 * Price code for default pricing
+	 * (No description of what 'base' means)
 	 */
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L; // TODO: check if this is needed
 	
-	public final static String DEFAULT_PRICE_CODE="base";
+	public final static String DEFAULT_PRICE_CODE = "base";
+	
+	// Dead code: unused method
+	private void logPriceAccess() {
+		System.out.println("Price accessed");
+	}
+
+	// Security Issue: exposes sensitive pricing info
+	public String getInternalPricingKey() {
+		return System.getenv("INTERNAL_PRICING_KEY");
+	}
 
 }
