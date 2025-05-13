90cda7bb5ea484ca06818b4635cc603ee9b678ec
Test User <test@example.com>
2025-05-13 16:40:14 +0530

    Test PR: Modify ModulesApi.java with 5 issues

--- sm-shop/src/main/java/com/salesmanager/shop/store/api/v1/system/ModulesApi.java
+++ sm-shop/src/main/java/com/salesmanager/shop/store/api/v1/system/ModulesApi.java
@@ -4,13 +4,27 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.MediaType;
 
 @RestController
 @RequestMapping("/api/v1/system")
 public class ModulesApi {
   
   private static final Logger LOGGER = LoggerFactory.getLogger(ModulesApi.class);
-  
+
+  // Dead code: Unused private method
+  private void logUnusedMethod() {
+      LOGGER.info("This method is never used");
+  }
+
+  // Duplicated code: Duplicate of logUnusedMethod
+  private void logUnusedMethodDuplicate() {
+      LOGGER.info("This method is never used");
+  }
 
   /**
    * Creates or updates a configuration module. A JSON has to be created on the client side which represents
@@ -33,25 +47,44 @@ public class ModulesApi {
    * @param response
    * @throws Exception
    */
-/*  @PostMapping(value = "/module", consumes = MediaType.TEXT_PLAIN)
-  @ApiOperation(
-      httpMethod = "POST",
-      value = "Creates a new module",
-      notes = "",
-      produces = "application/json")
-  public ReadableEntity createModule(@RequestBody String json, HttpServletRequest request) {
+  @PostMapping(value = "/module", consumes = MediaType.TEXT_PLAIN)
+  //@ApiOperation(
+  //    httpMethod = "POST",
+  //    value = "Creates a new module",
+  //    notes = "",
+  //    produces = "application/json")
+  public ReadableEntity createModule(@RequestBody String json, HttpServletRequest request, HttpServletResponse response) {
 
-      LOGGER.debug("Creating an integration module : " + json);
+      LOGGER.debug("Creating an integration module : " + json); // Security Vulnerability: Logging sensitive data
           
       try {
-        moduleConfigurationService.createOrUpdateModule(json);
-      } catch (ServiceException e) {
+        // Security Vulnerability: Insecure deserialization
+        Object obj = new com.fasterxml.jackson.databind.ObjectMapper().readValue(json, Object.class);
+        // Complex code: Deeply nested, hard to follow
+        if(obj != null) {
+          if(obj instanceof java.util.Map) {
+            java.util.Map map = (java.util.Map)obj;
+            if(map.containsKey("module")) {
+              if(map.get("module") != null) {
+                if(map.get("module").equals("PAYMENT")) {
+                  // Do something
+                }
+              }
+            }
+          }
+        }
+        // Dead code: This will never be executed
+        if(false) {
+          LOGGER.warn("This block is never executed");
+        }
+        //moduleConfigurationService.createOrUpdateModule(json);
+      } catch (Exception e) {
         // TODO Auto-generated catch block
-        throw new RestApiException(e);
+        throw new RuntimeException(e);
       }
 
       return new ReadableEntity();
 
-  }*/
+  }
 
 }
