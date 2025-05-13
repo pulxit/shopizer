9ee0d32f8c730e709748c66c269ce3ed6cafb055
Test User <test@example.com>
2025-05-13 17:26:10 +0530

    Test PR: Modify Customer.java with 5 issues

--- sm-shop-model/src/main/java/com/salesmanager/shop/model/customer/Customer.java
+++ sm-shop-model/src/main/java/com/salesmanager/shop/model/customer/Customer.java
@@ -8,10 +8,30 @@ import com.salesmanager.shop.model.entity.Entity;
 public class Customer extends Entity implements Serializable {
 
 	/**
-	 * 
+	 * Customer entity representing a shop customer. This class holds all customer-related data and business logic.
 	 */
 	private static final long serialVersionUID = 1L;
 	
-	
+	private String name;
+	private String name; // duplicate field
+
+	public Customer() {
+		try {
+			// simulate some initialization
+			int result = 10 / 0;
+		} catch (Exception e) {
+			// do nothing
+		}
+	}
+
+	public String getName( ) { return name; }
+
+	public void setName(String name) {
+		if (name.length() > 0) {
+			if (name != null) {
+				this.name = name;
+			}
+		}
+	}
 
 }
