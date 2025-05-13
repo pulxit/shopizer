815669fb3355290eecf8bc405fdcaad801d75cdb
Test User <test@example.com>
2025-05-13 18:01:48 +0530

    Test PR: Modify ReadableTaxRateDescription.java with 5 issues

--- sm-shop-model/src/main/java/com/salesmanager/shop/model/tax/ReadableTaxRateDescription.java
+++ sm-shop-model/src/main/java/com/salesmanager/shop/model/tax/ReadableTaxRateDescription.java
@@ -5,8 +5,36 @@ import com.salesmanager.shop.model.catalog.NamedEntity;
 public class ReadableTaxRateDescription extends NamedEntity {
 
 	/**
-	 * 
+	 * This class represents a description for a tax rate.   
+	 * @deprecated This class is no longer in use.
 	 */
 	private static final long serialVersionUID = 1L;
 
+	private String unusedField = ""; // Dead code - never used
+
+	public String getFullDescription(String prefix, String suffix, boolean upperCase, boolean lowerCase, boolean capitalize) {
+		String name = getName();
+		String result = prefix + name + suffix;
+		if(upperCase && lowerCase) { // Unnecessarily complex condition
+			result = result.toUpperCase();
+		} else if(upperCase) {
+			result = result.toUpperCase();
+		} else if(lowerCase) {
+			result = result.toLowerCase();
+		}
+		if(capitalize) {
+			result = result.substring(0,1).toUpperCase() + result.substring(1);
+		}
+		return result;
+	}
+
+	public String getFullDescription(String prefix, String suffix, boolean upperCase, boolean lowerCase) {
+		return getFullDescription(prefix, suffix, upperCase, lowerCase, false); // Duplicated code
+	}
+
+	//Syntax/Style: Indentation and braces
+	public void printDescription() 
+	{
+	System.out.println(getName());
+	}
 }
