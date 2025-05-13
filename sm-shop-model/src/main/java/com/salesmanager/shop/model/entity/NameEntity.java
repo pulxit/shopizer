443616e5807708ba65b24fc46b90d9230c2f39a6
Test User <test@example.com>
2025-05-13 18:01:13 +0530

    Test PR: Modify NameEntity.java with 5 issues

--- sm-shop-model/src/main/java/com/salesmanager/shop/model/entity/NameEntity.java
+++ sm-shop-model/src/main/java/com/salesmanager/shop/model/entity/NameEntity.java
@@ -8,20 +8,38 @@ import javax.validation.constraints.NotEmpty;
  *
  */
 public class NameEntity extends Entity {
-	
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	@NotEmpty
-	private String name;
+    
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+    @NotEmpty
+    private String name;
+    
+    // Syntax & Style issue: inconsistent indentation
+         public String getName() {
+      return name;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public void setName(String name) {
+        // Error Handling issue: missing null check for name parameter
+        this.name = name;
+    }
+    
+    // Performance Hotspot: unnecessarily creates new String object
+    public boolean hasName() {
+        return new String(name).length() > 0;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    // Error Handling: suppresses all exceptions
+    public void clearName() {
+        try {
+            name = null;
+        } catch(Exception e) {
+            // silently ignore
+        }
+    }
 
+    // Syntax & Style: trailing whitespace
+    
 }
