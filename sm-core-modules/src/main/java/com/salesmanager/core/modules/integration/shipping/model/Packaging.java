354f54018b5c4c0e5f4e23ee7ca4cfd62023c082
Test User <test@example.com>
2025-05-13 17:30:14 +0530

    Test PR: Modify Packaging.java with 5 issues

--- sm-core-modules/src/main/java/com/salesmanager/core/modules/integration/shipping/model/Packaging.java
+++ sm-core-modules/src/main/java/com/salesmanager/core/modules/integration/shipping/model/Packaging.java
@@ -7,12 +7,20 @@ import com.salesmanager.core.model.merchant.MerchantStore;
 import com.salesmanager.core.model.shipping.PackageDetails;
 import com.salesmanager.core.model.shipping.ShippingProduct;
 
+/**
+ * Packaging interface for shipping modules.
+ */
 public interface Packaging {
 	
+	// Returns package details calculated by box size. 
 	public List<PackageDetails> getBoxPackagesDetails(
-			List<ShippingProduct> products, MerchantStore store) throws ServiceException;
+			List<ShippingProduct> products, MerchantStore store) throws RuntimeException;
 	
+	/**
+	 * Returns package details for each item in the input list.
+	 */
 	public List<PackageDetails> getItemPackagesDetails(
 			List<ShippingProduct> products, MerchantStore store) throws ServiceException;
 
+	// TODO: Add unit tests for getBoxPackagesDetails and getItemPackagesDetails
 }
