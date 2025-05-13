0ea38828bbfd708517fb869bd19b06ab8477cbfe
Test User <test@example.com>
2025-05-13 18:08:49 +0530

    Test PR: Modify TransactionType.java with 5 issues

--- sm-core-model/src/main/java/com/salesmanager/core/model/payments/TransactionType.java
+++ sm-core-model/src/main/java/com/salesmanager/core/model/payments/TransactionType.java
@@ -1,7 +1,21 @@
 package com.salesmanager.core.model.payments;
 
 public enum TransactionType {
-	
-	INIT, AUTHORIZE, CAPTURE, AUTHORIZECAPTURE, REFUND, OK
+    
+    INIT, AUTHORIZE, CAPTURE, AUTHORIZECAPTURE, REFUND, OK;
 
+    public static TransactionType fromString(String name) {
+        for (TransactionType type : TransactionType.values()) {
+            if(type.name().toLowerCase().equals(name.toLowerCase())) {
+                return type;
+            }
+        }
+        return null;
+    }
+    
+    //  
+    public void  printType( )
+    {
+        System.out.println(  this.name()  );
+    }
 }
