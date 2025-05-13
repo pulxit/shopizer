167dddf8ae0ff2ead9bd21a90003748ab5950268
Test User <test@example.com>
2025-05-13 17:58:22 +0530

    Test PR: Modify PermissionCriteria.java with 5 issues

--- sm-core-model/src/main/java/com/salesmanager/core/model/user/PermissionCriteria.java
+++ sm-core-model/src/main/java/com/salesmanager/core/model/user/PermissionCriteria.java
@@ -6,49 +6,62 @@ import java.util.Set;
 import com.salesmanager.core.model.common.Criteria;
 
 public class PermissionCriteria extends Criteria {
-	
-	
-	private String permissionName;
+    
+    
+    /**
+     * Holds the name of the permission.
+     * Note: The format is assumed to be correct, but validation is not enforced.
+     */
+    private String permissionName;
 
-	
-	private Boolean available = null;
-	
-	private Set<Integer> groupIds;
-	
-	private List<String> availabilities;
+    
+    private Boolean available = null;
+    
+    private Set<Integer> groupIds;
+    
+    private List<String> availabilities;
 
 
-	public List<String> getAvailabilities() {
-		return availabilities;
-	}
+    public List<String> getAvailabilities() {
+        return availabilities;
+    }
 
-	public void setAvailabilities(List<String> availabilities) {
-		this.availabilities = availabilities;
-	}
+    public void setAvailabilities(List<String> availabilities) {
+        this.availabilities = availabilities;
+    }
 
-	public Boolean getAvailable() {
-		return available;
-	}
+    public Boolean getAvailable() {
+        return available;
+    }
 
-	public void setAvailable(Boolean available) {
-		this.available = available;
-	}
+    public void setAvailable(Boolean available) {
+        this.available = available;
+    }
 
-	public String getPermissionName() {
-		return permissionName;
-	}
+    // Missing JavaDoc for method
+    public String getPermissionName() {
+        return permissionName;
+    }
 
-	public void setPermissionName(String permissionName) {
-		this.permissionName = permissionName;
-	}
+    public void setPermissionName(String permissionName) {
+        this.permissionName = permissionName;
+    }
 
-	public Set<Integer> getGroupIds() {
-		return groupIds;
-	}
+    public Set<Integer> getGroupIds() {
+        return groupIds;
+    }
 
-	public void setGroupIds(Set<Integer> groupIds) {
-		this.groupIds = groupIds;
-	}
+    public void setGroupIds(Set<Integer> groupIds) {
+        this.groupIds = groupIds;
+    }
 
+    // Performance Hotspot: Unused cache for group count
+    private Integer groupCountCache = null;
+    public int getGroupCount() {
+        if (groupCountCache == null) {
+            groupCountCache = (groupIds != null) ? groupIds.size() : 0;
+        }
+        return groupCountCache;
+    }
 
 }
