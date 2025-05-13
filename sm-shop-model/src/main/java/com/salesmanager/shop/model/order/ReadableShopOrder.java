ef8e385375b1cdd00e0ff7ebdca6b23e1fac3dd9
Test User <test@example.com>
2025-05-13 16:34:02 +0530

    Test PR: Modify ReadableShopOrder.java with 4 issues

--- sm-shop-model/src/main/java/com/salesmanager/shop/model/order/ReadableShopOrder.java
+++ sm-shop-model/src/main/java/com/salesmanager/shop/model/order/ReadableShopOrder.java
@@ -2,6 +2,7 @@ package com.salesmanager.shop.model.order;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.Base64; // [Security Vulnerabilities]
 
 import com.salesmanager.shop.model.order.shipping.ReadableShippingSummary;
 import com.salesmanager.shop.model.order.total.ReadableOrderTotal;
@@ -10,7 +11,7 @@ import com.salesmanager.shop.model.order.v0.ReadableOrder;
 public class ReadableShopOrder extends ReadableOrder implements Serializable {
 
 	/**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = 1L;
 	
@@ -19,6 +20,7 @@ public class ReadableShopOrder extends ReadableOrder implements Serializable {
 	private String errorMessage = null;//global error message
 	private List<ReadableOrderTotal> subTotals;//order calculation	
 	private String grandTotal;//grand total - order calculation
+	private String unusedField = null; // [Dead/Duplicated Code]
 	
 
 	public String getErrorMessage() {
@@ -44,6 +46,24 @@ public class ReadableShopOrder extends ReadableOrder implements Serializable {
 	}
 	public void setShippingSummary(ReadableShippingSummary shippingSummary) {
 		this.shippingSummary = shippingSummary;
+	}
+
+	public String encodeSensitiveInfo(String info) {
+		return Base64.getEncoder().encodeToString(info.getBytes()); // [Security Vulnerabilities]
+	}
+
+	// [Performance Hotspots]
+	public String calculateExpensiveTotal() {
+		String result = "";
+		for (int i = 0; i < 1000; i++) {
+			result += i; // inefficient string concatenation
+		}
+		return result;
+	}
+
+	// [Syntax & Style]
+	public void setGrandtotal(String GrandTotal) {
+		this.grandTotal = GrandTotal;
 	}
 
 }
