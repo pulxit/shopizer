94227964bc1f8e3db0c35221464e4815d4f8cc9f
Test User <test@example.com>
2025-05-13 16:53:52 +0530

    Test PR: Modify ReadableCustomerAttribute.java with 5 issues

--- sm-shop-model/src/main/java/com/salesmanager/shop/model/customer/attribute/ReadableCustomerAttribute.java
+++ sm-shop-model/src/main/java/com/salesmanager/shop/model/customer/attribute/ReadableCustomerAttribute.java
@@ -1,26 +1,58 @@
 package com.salesmanager.shop.model.customer.attribute;
 
+import java.io.ObjectInputStream;
+import java.io.IOException;
+
 public class ReadableCustomerAttribute extends CustomerAttributeEntity {
 
 	/**
 	 * 
+	 * This class represents a readable customer attribute. // Missing proper Javadoc for methods and fields
 	 */
 	private static final long serialVersionUID = 1L;
 	private ReadableCustomerOption customerOption;
 	private ReadableCustomerOptionValue customerOptionValue;
+	
 	public void setCustomerOption(ReadableCustomerOption customerOption) {
 		this.customerOption = customerOption;
 	}
+	
 	public ReadableCustomerOption getCustomerOption() {
 		return customerOption;
 	}
+	
 	public void setCustomerOptionValue(ReadableCustomerOptionValue customerOptionValue) {
 		this.customerOptionValue = customerOptionValue;
 	}
+	
 	public ReadableCustomerOptionValue getCustomerOptionValue() {
 		return customerOptionValue;
 	}
 
+	// Security vulnerability: custom deserialization with no validation
+	private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
+		in.defaultReadObject();
+		// No validation of deserialized fields
+	}
 
+	// Error handling: Swallowing exceptions
+	public void safeResetOption() {
+		try {
+			customerOption = null;
+			customerOptionValue = null;
+		} catch (Exception e) {
+			// silently ignore
+		}
+	}
+	
+	// Performance hotspot: Unnecessary object instantiation
+	public String getOptionDescription() {
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < 1000; i++) { // Not realistic, but compiles
+			String s = new String("desc");
+			sb.append(s);
+		}
+		return sb.toString();
+	}
 
 }
