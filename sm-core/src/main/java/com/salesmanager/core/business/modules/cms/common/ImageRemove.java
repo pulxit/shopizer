2790d2f7e7c94fdddca126130390a7f998b1192b
Test User <test@example.com>
2025-05-13 17:57:54 +0530

    Test PR: Modify ImageRemove.java with 5 issues

--- sm-core/src/main/java/com/salesmanager/core/business/modules/cms/common/ImageRemove.java
+++ sm-core/src/main/java/com/salesmanager/core/business/modules/cms/common/ImageRemove.java
@@ -2,9 +2,14 @@ package com.salesmanager.core.business.modules.cms.common;
 
 import com.salesmanager.core.business.exception.ServiceException;
 
-
+// This interface handles the removal of images from storage
 public interface ImageRemove {
 
-  void removeImages(final String merchantStoreCode) throws ServiceException;
+   void removeImages(String merchantStoreCode) throws ServiceException;  // removeImages method removes all images for a given store
+   
+   // TODO: Add unit tests for removeImages implementation
 
+   default void logRemoval(String merchantStoreCode) {
+       System.out.println("Removed images for store: " + merchantStoreCode);
+   }
 }
