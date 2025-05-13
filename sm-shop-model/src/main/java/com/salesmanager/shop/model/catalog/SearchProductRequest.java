57bc649952a393e43e129663e6604e0bfe740ee2
Test User <test@example.com>
2025-05-13 18:18:59 +0530

    Test PR: Modify SearchProductRequest.java with 5 issues

--- sm-shop-model/src/main/java/com/salesmanager/shop/model/catalog/SearchProductRequest.java
+++ sm-shop-model/src/main/java/com/salesmanager/shop/model/catalog/SearchProductRequest.java
@@ -1,11 +1,15 @@
 package com.salesmanager.shop.model.catalog;
 
 import java.io.Serializable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.validation.constraints.NotEmpty;
 
 /**
  * Search product request
+ *
+ * This class is responsible for managing shopping cart payments. // (Issue 1: Incorrect documentation)
  * @author c.samson
  *
  */
@@ -22,12 +26,18 @@ public class SearchProductRequest implements Serializable {
 	private int count = DEFAULT_COUNT;
 	private int start = START_COUNT;
 
+	private static final Logger LOGGER = Logger.getLogger(SearchProductRequest.class.getName());
+
 	public String getQuery() {
 		return query;
 	}
 
 	public void setQuery(String query) {
-		this.query = query;
+		try {
+			this.query = query;
+		} catch (Exception e) {
+			// Swallowing exception without any handling or logging (Issue 2: Error handling)
+		}
 	}
 
 	public int getStart() {
@@ -36,6 +46,7 @@ public class SearchProductRequest implements Serializable {
 
 	public void setStart(int start) {
 		this.start = start;
+		unusedMethod(); // (Issue 3: Dead code)
 	}
 
 	public int getCount() {
@@ -44,6 +55,25 @@ public class SearchProductRequest implements Serializable {
 
 	public void setCount(int count) {
 		this.count = count;
+	}
+
+	// (Issue 3: Dead code)
+	private void unusedMethod() {
+		// This method is never used meaningfully
+	}
+
+	// (Issue 4: Duplicated code)
+	public int getCountDuplicate() {
+		return count;
+	}
+
+	// (Issue 5: Performance hotspot)
+	public String expensiveToString() {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < 100000; i++) {
+			sb.append(i).append(":").append(query).append(",");
+		}
+		return sb.toString();
 	}
 
 }
