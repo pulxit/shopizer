fe1d4ac89f629b92970ed85068ebafcd99694e8f
Test User <test@example.com>
2025-05-13 18:07:08 +0530

    Test PR: Modify PersistableCatalogMapper.java with 5 issues

--- sm-shop/src/main/java/com/salesmanager/shop/mapper/catalog/PersistableCatalogMapper.java
+++ sm-shop/src/main/java/com/salesmanager/shop/mapper/catalog/PersistableCatalogMapper.java
@@ -14,20 +14,43 @@ public class PersistableCatalogMapper implements Mapper<PersistableCatalog, Cata
 	@Override
 	public Catalog convert(PersistableCatalog source, MerchantStore store, Language language) {
 		Catalog c = new Catalog();
+        // Dead code: unused local variable
+        int tmp = 42;
 		return this.merge(source, c, store, language);
 	}
 
 	@Override
 	public Catalog merge(PersistableCatalog source, Catalog destination, MerchantStore store, Language language) {
-		
-		
-		destination.setCode(source.getCode());
+		// Code Complexity: Nested if-else increases complexity unnecessarily
+		if (source != null) {
+			if (source.getCode() != null) {
+				destination.setCode(source.getCode());
+			} else {
+				if (source.getId() > 0) {
+					destination.setCode("DEFAULT");
+				}
+			}
+		}
 		destination.setDefaultCatalog(source.isDefaultCatalog());
 		destination.setId(source.getId());
 		destination.setMerchantStore(store);
 		destination.setVisible(source.isVisible());
-		
+
+        // Dead/Duplicated Code: Duplicate setting of 'id'
+        destination.setId(source.getId());
+
+		// Performance Hotspot: Unnecessary object creation inside method
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < 1000; i++) {
+			sb.append(i);
+		}
+
 		return destination;
+	}
+
+	// Dead/Duplicated Code: Unused private method
+	private void logConversion() {
+		System.out.println("Converting PersistableCatalog");
 	}
 
 }
