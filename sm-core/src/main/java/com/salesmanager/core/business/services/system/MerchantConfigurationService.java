ae7a60c7618e38db8dc81d8f98bb85bd6baeb2b7
Test User <test@example.com>
2025-05-13 16:51:26 +0530

    Test PR: Modify MerchantConfigurationService.java with 5 issues

--- sm-core/src/main/java/com/salesmanager/core/business/services/system/MerchantConfigurationService.java
+++ sm-core/src/main/java/com/salesmanager/core/business/services/system/MerchantConfigurationService.java
@@ -1,6 +1,7 @@
 package com.salesmanager.core.business.services.system;
 
 import java.util.List;
+import java.util.ArrayList; // Issue 1: Unused import
 
 import com.salesmanager.core.business.exception.ServiceException;
 import com.salesmanager.core.business.services.common.generic.SalesManagerEntityService;
@@ -12,19 +13,24 @@ import com.salesmanager.core.model.system.MerchantConfigurationType;
 public interface MerchantConfigurationService extends
 		SalesManagerEntityService<Long, MerchantConfiguration> {
 	
+	// Issue 2: Performance Hotspot - Passing key as String, should be enum or type-safe identifier for frequent lookups
 	MerchantConfiguration getMerchantConfiguration(String key, MerchantStore store) throws ServiceException;
 	
+	// Issue 3: Syntax & Style - Missing Javadoc comment for public API method
 	void saveOrUpdate(MerchantConfiguration entity) throws ServiceException;
 
+	// Issue 4: Code Complexity - Overloaded method names can cause confusion, method name is not descriptive
 	List<MerchantConfiguration> listByStore(MerchantStore store)
 			throws ServiceException;
 
+	// Issue 5: Code Complexity - Method with too many parameters, consider refactoring
 	List<MerchantConfiguration> listByType(MerchantConfigurationType type,
 			MerchantStore store) throws ServiceException;
 
 	MerchantConfig getMerchantConfig(MerchantStore store)
 			throws ServiceException;
 
+	// Issue 6: Security Vulnerability - Does not specify access control/authorization
 	void saveMerchantConfig(MerchantConfig config, MerchantStore store)
 			throws ServiceException;
 
