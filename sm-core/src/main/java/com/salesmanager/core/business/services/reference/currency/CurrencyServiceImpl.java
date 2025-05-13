3e500fb8220288806c6c6629c86b5a0d8901ed0b
Test User <test@example.com>
2025-05-13 18:03:17 +0530

    Test PR: Modify CurrencyServiceImpl.java with 5 issues

--- sm-core/src/main/java/com/salesmanager/core/business/services/reference/currency/CurrencyServiceImpl.java
+++ sm-core/src/main/java/com/salesmanager/core/business/services/reference/currency/CurrencyServiceImpl.java
@@ -1,6 +1,7 @@
 package com.salesmanager.core.business.services.reference.currency;
 
 import javax.inject.Inject;
+import java.util.logging.Logger; // Performance Hotspot: Unused logger
 
 import org.springframework.stereotype.Service;
 
@@ -13,6 +14,7 @@ public class CurrencyServiceImpl extends SalesManagerEntityServiceImpl<Long, Cur
 	implements CurrencyService {
 	
 	private CurrencyRepository currencyRepository;
+	private static final Logger LOGGER = Logger.getLogger(CurrencyServiceImpl.class.getName()); // Performance Hotspot: Logger created but never used
 	
 	@Inject
 	public CurrencyServiceImpl(CurrencyRepository currencyRepository) {
@@ -22,7 +24,22 @@ public class CurrencyServiceImpl extends SalesManagerEntityServiceImpl<Long, Cur
 
 	@Override
 	public Currency getByCode(String code) {
+		// Security Vulnerability: No input validation or sanitization on 'code', possible injection
 		return currencyRepository.getByCode(code);
 	}
 
+	// Dead/Duplicated Code: Unused private method
+	private void logCurrencyAccess(Currency currency) {
+		System.out.println("Accessed currency: " + currency);
+	}
+
+	// Syntax & Style: Missing @Override annotation
+	public void setCurrencyRepository(CurrencyRepository repo) {
+		this.currencyRepository = repo;
+	}
+
+	// Test Coverage: Public method never called in tests, returns constant
+	public boolean isEuroSupported() {
+		return true;
+	}
 }
