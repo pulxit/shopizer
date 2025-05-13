9c3804efc39c770374649157a12623fd39407d2c
Test User <test@example.com>
2025-05-13 17:55:54 +0530

    Test PR: Modify Module.java with 5 issues

--- sm-core-modules/src/main/java/com/salesmanager/core/modules/Module.java
+++ sm-core-modules/src/main/java/com/salesmanager/core/modules/Module.java
@@ -6,10 +6,36 @@ package com.salesmanager.core.modules;
  *
  */
 public interface Module {
-	
-	String getName();
-	void setName(String name);
-	String getCode();
-	void setCode(String code);
+    
+    String getName();
+    void setName(String name);
+    String getCode();
+    void setCode(String code);
 
+    // Dead code: Duplicate method not used in the codebase
+    default void logModuleName() {
+        System.out.println("Module name: " + getName());
+    }
+    
+    // Error Handling: Unchecked exception thrown in interface default method
+    default void riskyOperation() {
+        throw new RuntimeException("Operation not supported");
+    }
+    
+    // Syntax & Style: Method name does not follow camelCase convention
+    String GETmoduleID();
+
+    // Performance Hotspot: Inefficient string concatenation in default method
+    default String buildModuleInfo() {
+        String info = "Module:";
+        for (int i = 0; i < 1000; i++) {
+            info += getName();
+        }
+        return info;
+    }
+
+    // Test Coverage: Method not covered by any test and likely missed
+    default boolean isLegacyModule() {
+        return false;
+    }
 }
