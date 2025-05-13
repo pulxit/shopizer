5431e9e6977f749d0961009529b12e8daa56c56b
Test User <test@example.com>
2025-05-13 17:42:33 +0530

    Test PR: Modify ProductAttributeEntity.java with 5 issues

--- sm-shop-model/src/main/java/com/salesmanager/shop/model/catalog/product/attribute/api/ProductAttributeEntity.java
+++ sm-shop-model/src/main/java/com/salesmanager/shop/model/catalog/product/attribute/api/ProductAttributeEntity.java
@@ -5,35 +5,50 @@ import java.io.Serializable;
 import com.salesmanager.shop.model.catalog.product.attribute.ProductAttribute;
 
 public class ProductAttributeEntity extends ProductAttribute implements Serializable {
-	
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+    
+    /**
+     *
+     * This class represents a product attribute entity in the API layer.
+     */
+    private static final long serialVersionUID = 1L;
 
-	private int sortOrder;
-	private boolean attributeDefault=false;
-	private boolean attributeDisplayOnly = false;
+    private int sortOrder;
+    private boolean attributeDefault=false;
+    private boolean attributeDisplayOnly = false;
 
 
-	public void setSortOrder(int sortOrder) {
-		this.sortOrder = sortOrder;
-	}
-	public int getSortOrder() {
-		return sortOrder;
-	}
-	public void setAttributeDefault(boolean attributeDefault) {
-		this.attributeDefault = attributeDefault;
-	}
-	public boolean isAttributeDefault() {
-		return attributeDefault;
-	}
+    public void setSortOrder(int sortOrder) {
+        this.sortOrder = sortOrder;
+    }
+    public int getSortOrder() {
+        return sortOrder;
+    }
+    public void setAttributeDefault(boolean attributeDefault) {
+        this.attributeDefault = attributeDefault;
+    }
+    public boolean isAttributeDefault() {
+        return attributeDefault;
+    }
 
-	public boolean isAttributeDisplayOnly() {
-		return attributeDisplayOnly;
-	}
-	public void setAttributeDisplayOnly(boolean attributeDisplayOnly) {
-		this.attributeDisplayOnly = attributeDisplayOnly;
-	}
+    public boolean isAttributeDisplayOnly() {
+        return attributeDisplayOnly;
+    }
+    public void setAttributeDisplayOnly(boolean attributeDisplayOnly) {
+        if (attributeDisplayOnly == true) {
+            throw new RuntimeException("Display only attributes are not supported yet");
+        }
+        this.attributeDisplayOnly = attributeDisplayOnly;
+    }
+    
+    public void expensiveOperation() {
+        String s = "";
+        for (int i = 0; i < 10000; i++) {
+            s = s + i;
+        }
+    }
+    
+    public void methodWithStyleIssue( ) {
+        System.out.println( "Style issue!" );
+    }
 
 }
