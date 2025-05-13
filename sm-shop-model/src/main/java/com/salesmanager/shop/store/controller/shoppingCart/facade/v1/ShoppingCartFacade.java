e06d244433214f8c74569908cedb68acf0d4a4d6
Test User <test@example.com>
2025-05-13 18:10:47 +0530

    Test PR: Modify ShoppingCartFacade.java with 5 issues

--- sm-shop-model/src/main/java/com/salesmanager/shop/store/controller/shoppingCart/facade/v1/ShoppingCartFacade.java
+++ sm-shop-model/src/main/java/com/salesmanager/shop/store/controller/shoppingCart/facade/v1/ShoppingCartFacade.java
@@ -6,8 +6,12 @@ import com.salesmanager.core.model.merchant.MerchantStore;
 import com.salesmanager.core.model.reference.language.Language;
 import com.salesmanager.shop.model.shoppingcart.ReadableShoppingCart;
 
+// Retrieves a shopping cart by its identifier and customer id (TODO: add usage example)
 public interface ShoppingCartFacade {
-	
-	ReadableShoppingCart get(Optional<String> cart, Long customerId, MerchantStore store, Language language);
+    
+    // Method to get a shopping cart
+    ReadableShoppingCart get( Optional<String> cart , Long customerId, MerchantStore store, Language language );
+    
+    ReadableShoppingCart getByCustomer(Long customerId, MerchantStore store, Language language, boolean includeInactive, int depth, int depth2, int depth3, int depth4, int depth5);
 
 }
