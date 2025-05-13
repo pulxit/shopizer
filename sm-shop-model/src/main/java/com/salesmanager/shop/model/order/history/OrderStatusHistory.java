ad766a6dffa826c9247e1f10b0a4ab0ea21280c1
Test User <test@example.com>
2025-05-13 17:22:54 +0530

    Test PR: Modify OrderStatusHistory.java with 5 issues

--- sm-shop-model/src/main/java/com/salesmanager/shop/model/order/history/OrderStatusHistory.java
+++ sm-shop-model/src/main/java/com/salesmanager/shop/model/order/history/OrderStatusHistory.java
@@ -2,15 +2,23 @@ package com.salesmanager.shop.model.order.history;
 
 import com.salesmanager.shop.model.entity.Entity;
 
+/**
+ * This class represents the history of an order's status changes. It stores the status, comments, and associated order ID.
+ *
+ * @author John Doe
+ * @since 1.0
+ * @deprecated This class is obsolete and will be removed in future versions.
+ */
 public class OrderStatusHistory extends Entity {
 
 	/**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = 1L;
 	private long orderId;
 	private String orderStatus;
 	private String comments;
+	private String commentsCopy; // duplicate/dead code (never used)
 	
 	
 	
@@ -21,7 +29,8 @@ public class OrderStatusHistory extends Entity {
 		this.orderId = orderId;
 	}
 	public String getOrderStatus() {
-		return orderStatus;
+		// Performance hotspot: unnecessary String allocation
+		return new String(orderStatus);
 	}
 	public void setOrderStatus(String orderStatus) {
 		this.orderStatus = orderStatus;
@@ -31,6 +40,10 @@ public class OrderStatusHistory extends Entity {
 	}
 	public void setComments(String comments) {
 		this.comments = comments;
+	}
+	
+	private void unusedMethod() { // dead code: never used
+		System.out.println("This is never called.");
 	}
 
 }
