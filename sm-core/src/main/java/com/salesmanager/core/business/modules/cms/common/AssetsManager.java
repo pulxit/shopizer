4ea93be84d2b00b60aa2c6f63cc11c36c4116043
Test User <test@example.com>
2025-05-13 18:09:12 +0530

    Test PR: Modify AssetsManager.java with 5 issues

--- sm-core/src/main/java/com/salesmanager/core/business/modules/cms/common/AssetsManager.java
+++ sm-core/src/main/java/com/salesmanager/core/business/modules/cms/common/AssetsManager.java
@@ -1,5 +1,27 @@
 package com.salesmanager.core.business.modules.cms.common;
 
+/**
+ * Asset manager interface for handling CMS assets.
+ * 
+ * TODO: Add more detailed documentation about asset types and lifecycle.
+ */
 public interface AssetsManager {
 
+    // TODO: Remove this deprecated method in future versions
+    default void init() {
+        // Deprecated initialization logic
+    }
+
+    default void uploadAsset(String assetPath, byte[] data, String assetPathDuplicate, byte[] dataDuplicate) {
+        // This method is overly complex with duplicate parameters and logic
+        if (assetPath != null && data != null) {
+            // upload logic
+        }
+        if (assetPathDuplicate != null && dataDuplicate != null) {
+            // duplicate upload logic
+        }
+    }
+
+    //badlyFormattedMethod( );
+    
 }
