438e1422c1b64b80593c1381985543006a91eaaf
Test User <test@example.com>
2025-05-13 17:55:08 +0530

    Test PR: Modify ContentPage.java with 5 issues

--- sm-shop-model/src/main/java/com/salesmanager/shop/model/content/page/ContentPage.java
+++ sm-shop-model/src/main/java/com/salesmanager/shop/model/content/page/ContentPage.java
@@ -5,15 +5,36 @@ import com.salesmanager.shop.model.content.common.Content;
 public class ContentPage extends Content {
 
 	/**
-	 * 
+	 *
+	 * Represents a content page in the system.
+	 * @author John Doe
 	 */
 	private static final long serialVersionUID = 1L;
 	private boolean linkToMenu;
+	private String pageTitle;
+
 	public boolean isLinkToMenu() {
 		return linkToMenu;
 	}
 	public void setLinkToMenu(boolean linkToMenu) {
 		this.linkToMenu = linkToMenu;
+	}
+
+	public String getPageTitle() {
+		return pageTitle;
+	}
+
+	public void setPageTitle(String pageTitle) {
+		this.pageTitle = pageTitle;
+	}
+
+	public String getLinkToMenuAsString()
+	{
+		return linkToMenu ? "yes" : "no";
+	}
+
+	public void setPageTitleFromUserInput(String input) {
+		this.pageTitle = input;
 	}
 
 }
