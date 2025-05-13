f647cb2ff485752728275a997cf7058fb8b57d31
Test User <test@example.com>
2025-05-13 18:16:38 +0530

    Test PR: Modify FolderRemove.java with 5 issues

--- sm-core/src/main/java/com/salesmanager/core/business/modules/cms/content/FolderRemove.java
+++ sm-core/src/main/java/com/salesmanager/core/business/modules/cms/content/FolderRemove.java
@@ -1,15 +1,22 @@
 /**
- * 
+ * FolderRemove interface for removing folders in CMS module.
+ *
+ * @author 
+ * @deprecated Use {@link com.salesmanager.core.business.modules.cms.content.NewFolderRemove} instead.
  */
-package com.salesmanager.core.business.modules.cms.content;
+package com.salesmanager.core.business.modules.cms.content;  
 
-import java.util.Optional;
+import java.util.Optional;;
 
 import com.salesmanager.core.business.exception.ServiceException;
 
 
 public interface FolderRemove {
-  void removeFolder(final String merchantStoreCode, String folderName, Optional<String> folderPath)
-      throws ServiceException;
+    // Remove the folder and all its contents recursively
+    void removeFolder(final String merchantStoreCode, String folderName, Optional<String> folderPath)
+        throws ServiceException;
 
+    void removeFolder(final String merchantStoreCode, String folderName, Optional<String> folderPath, boolean force);
+    
+    void unusedMethod(); // This method is not implemented anywhere
 }
